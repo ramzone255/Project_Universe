5496908b71ccb297d8295f9fbc4420d131db6f92
Ilya Prokaznikov <ramzone255@mail.ru>
2025-02-11 10:15:02 +0300

    Validator

--- Project_Universe.Backend/Project_Universe.Application/src/Common/Dependencies/DependencyInjection.cs
+++ Project_Universe.Backend/Project_Universe.Application/src/Common/Dependencies/DependencyInjection.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Project_Universe.Application.src.Common.Behaviors;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +17,10 @@ namespace Project_Universe.Application.src.Common.Dependencies
             this IServiceCollection services)
         {
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            services
+                .AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
+            services.AddTransient(typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehavior<,>));
             return services;
         }
     }
---
+++ Project_Universe.Backend/Project_Universe.Application/src/Entities/Staff/Commands/UpdateStaff/UpdateStaffCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Universe.Application.src.Entities.Staff.Commands.UpdateStaff
+{
+    public class UpdateStaffCommandValidator : AbstractValidator<UpdateStaffCommand>
+    {
+        public UpdateStaffCommandValidator()
+        {
+            RuleFor(updateEntityCommand =>
+            updateEntityCommand.name_staff).MaximumLength(50).NotEmpty();
+            RuleFor(updateEntityCommand =>
+            updateEntityCommand.lastname_staff).MaximumLength(50).NotEmpty();
+            RuleFor(updateEntityCommand =>
+            updateEntityCommand.patronymic_staff).MaximumLength(50).NotEmpty();
+            RuleFor(updateEntityCommand =>
+            updateEntityCommand.email_staff).MaximumLength(50).NotEmpty();
+            RuleFor(updateEntityCommand =>
+            updateEntityCommand.id_staff).NotEmpty();
+        }
+    }
+}
