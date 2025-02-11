d1e2505406a7926b3df8d03f95bac4dd15f16183
Ilya Prokaznikov <ramzone255@mail.ru>
2025-02-11 06:27:31 +0300

    Add Staff Commands

---
+++ Project_Universe.Backend/Project_Universe.Application/src/Entities/Staff/Commands/CreateStaff/CreateStaffCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Project_Universe.Application.src.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Universe.Application.src.Entities.Staff.Commands.CreateStaff
+{
+    public class CreateStaffCommandHandler
+        : IRequestHandler<CreateStaffCommand, int>
+    {
+        private readonly IProject_UniverseDbContext _dbContext;
+
+        public CreateStaffCommandHandler(IProject_UniverseDbContext dbContext) =>
+            _dbContext = dbContext;
+
+        public async Task<int> Handle(CreateStaffCommand request, CancellationToken cancellationToken)
+        {
+            var staffs = new Domain.src.Entities.Staff
+            {
+                name_staff = request.name_staff,
+                lastname_staff = request.lastname_staff,
+                patronymic_staff = request.patronymic_staff,
+                email_staff = request.email_staff,
+                id_post = request.id_post
+            };
+
+            await _dbContext.Staff.AddAsync(staffs, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return staffs.id_staff;
+        }
+    }
+}
---
+++ Project_Universe.Backend/Project_Universe.Application/src/Entities/Staff/Commands/DeleteStaff/DeleteStaffCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using MediatR.Pipeline;
+using Project_Universe.Application.src.Common.Exceptions;
+using Project_Universe.Application.src.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Universe.Application.src.Entities.Staff.Commands.DeleteStaff
+{
+    public class DeleteStaffCommandHandler
+        : IRequestHandler<DeleteStaffCommand>
+    {
+        private readonly IProject_UniverseDbContext _dbContext;
+
+        public DeleteStaffCommandHandler(IProject_UniverseDbContext dbContext) =>
+            _dbContext = dbContext;
+
+        public async Task Handle(DeleteStaffCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.Staff
+                .FindAsync(new object[] { request.id_staff }, cancellationToken);
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(Staff), request.id_staff);
+            }
+            _dbContext.Staff.Remove(entity);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return;
+        }
+    }
+}
