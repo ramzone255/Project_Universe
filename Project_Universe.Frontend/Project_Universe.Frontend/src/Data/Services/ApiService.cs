a45831749db2261b47026ec9be537c6219fb763a
Ilya Prokaznikov <ramzone255@mail.ru>
2025-02-13 16:02:44 +0300

    ApiService

---
+++ Project_Universe.Frontend/Project_Universe.Frontend/src/Data/Services/ApiService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Universe.Frontend.src.Data.Services
+{
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiService()
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:7248/") // Связь с Backend
+            };
+        }
+
+    }
+}
